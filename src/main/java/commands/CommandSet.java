a5490befceea3a01326e57a5ae1f31b31556ed48
Ponomarenko-Alice <a.ponomaremko111@gmail.com>
2023-05-29 23:59:51 +0300

    Add UpdateIdClass class

--- src/main/java/commands/CommandSet.java
+++ src/main/java/commands/CommandSet.java
@@ -11,17 +11,17 @@ public class CommandSet {
         commands.put("help", new HelpCommand(null, this));
         commands.put("info", new InfoCommand(collectionOfMusicBand, this));
         commands.put("show", new ShowCommand(collectionOfMusicBand, this));
-        commands.put("clear", new ClearCommand(collectionOfMusicBand, this));
         commands.put("save", new SaveCommand(collectionOfMusicBand, this));
-        commands.put("history", new HistoryCommand(null, this));
+        commands.put("clear", new ClearCommand(collectionOfMusicBand, this));
         commands.put("insert", new InsertCommand(collectionOfMusicBand, this));
+        commands.put("history", new HistoryCommand(null, this));
+        commands.put("update_id", new UpdateIdCommand(collectionOfMusicBand, this));
         commands.put("remove_key", new RemoveKeyCommand(collectionOfMusicBand, this));
         commands.put("remove_lower", new RemoveLowerKeyCommand(collectionOfMusicBand, this));
         commands.put("remove_greater_key", new RemoveGreaterCommand(collectionOfMusicBand, this));
         commands.put("remove_all_by_genre", new RemoveAllByGenre(collectionOfMusicBand, this));
-        commands.put("group_counting_by_name", new GroupByNameCommand(collectionOfMusicBand, this));
         commands.put("count_less_than_genre", new CountLessThanGenre(collectionOfMusicBand, this));
-
+        commands.put("group_counting_by_name", new GroupByNameCommand(collectionOfMusicBand, this));
 
     }
 
---
+++ src/main/java/commands/UpdateIdCommand.java
@@ -0,0 +1,46 @@
+package commands;
+
+import collection.CollectionOfMusicBand;
+import collection.MusicBand;
+
+import java.util.Arrays;
+
+public class UpdateIdCommand extends Command {
+    public UpdateIdCommand(CollectionOfMusicBand collectionOfMusicBand, CommandSet commands, String... params) {
+        super(collectionOfMusicBand, commands, params);
+    }
+
+    @Override
+    public void execute() {
+
+        if (params.length == 0) {
+            System.out.println("No card with this key found");
+        } else {
+            String param = Arrays.stream(params).toList().get(0);
+            try {
+                Long id = Long.parseLong(param);
+                if (collectionOfMusicBand.getCollectionOfCards().get(id) != null) {
+                    collectionOfMusicBand.getCollectionOfCards().remove(id);
+
+                    CommandManager commandManager = new CommandManager();
+                    MusicBand musicBand = commandManager.getNewCard(collectionOfMusicBand, params);
+                    collectionOfMusicBand.addMusicBand(musicBand.getId(), musicBand);
+                    System.out.println("Card update! :)");
+
+                } else {
+                    System.out.println("No card with this key found");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Key format must be Long");
+            }
+        }
+    }
+    @Override
+    public String getName() {
+        return "update_id";
+    }
+
+    @Override
+    public String getDescription() {
+        return " update card by its key";}
+}
